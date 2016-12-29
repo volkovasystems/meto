1709af5cfb644a330c90342677ebf2d8b61b4431
johnlenon <johnlenonmaghanoy@gmail.com>
2016-12-29 20:30:12 +0800

    Improved codes.

--- meto.js
+++ meto.js
@@ -32,6 +32,9 @@
 			"file": "meto.js",
 			"module": "meto",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/meto.git",
 			"test": "meto-test.js",
