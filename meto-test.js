0f90107d65f70bde805159407221b1abd9604c62
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 15:39:57 +0800

    Updated test.

--- meto-test.js
+++ meto-test.js
@@ -1,8 +1,8 @@
 const assert = require( "assert" );
 const meto = require( "./meto.js" );
 
-assert.ok( JSON.stringify( meto( "name", { "name": "simple" } ) ) );
+assert.ok( JSON.stringify( meto( "name", { "name": "simple" } ) ), "should not throw error" );
 
-assert.equal( Object.isFrozen( meto( "name", { "name": "simple" } ) ), true, "should return true" );
+assert.equal( Object.isFrozen( meto( "name", { "name": "simple" } ) ), true, "should be equal to true" );
 
 console.log( "ok" );
